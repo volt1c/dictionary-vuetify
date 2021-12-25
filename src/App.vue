28c24ff98cf10dc8655f3be64e20c795e511dd0d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-25 14:06:42 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
