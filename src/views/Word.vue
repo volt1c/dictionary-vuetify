db5a8a94628dfbbc352e37f001a881d7e63bf774
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-29 21:47:01 +0100

    feat(`Word`): basic layout

--- src/views/Word.vue
+++ src/views/Word.vue
@@ -1,25 +1,73 @@
 <template>
   <div>
     <h1>{{ word }}</h1>
-    <ul>
-      <li v-for="(def, idx) in dictionary" :key="idx">
-        {{ JSON.stringify(def) }}
-      </li>
-    </ul>
+    <div v-if="isWord">
+      <ul>
+        <li v-for="(def, idx) in dictionary" :key="idx">
+          <h2>
+            {{ idx + 1 }}. {{ word }}
+            <span v-show="def.phonetic">[{{ def.phonetic }}]</span>
+            <span v-for="(phonetic, idx) in def.phonetics" :key="idx">
+              <button v-if="phonetic.audio" @click="play(phonetic.audio)">
+                <v-icon>mdi-volume-high</v-icon>
+              </button>
+            </span>
+          </h2>
+          <ul>
+            <li v-for="(meaning, idx) in def.meanings" :key="idx">
+              <ul>
+                <li
+                  type="1"
+                  v-for="(definition, idx) in meaning.definitions"
+                  :key="idx"
+                >
+                  <h4>&lt;&lt; {{ definition.definition }} &gt;&gt;</h4>
+                  <p v-if="definition.synonyms.length !== 0">
+                    <strong> synonyms: </strong>
+                    <router-link
+                      v-for="(synonym, idx) in definition.synonyms"
+                      :key="idx"
+                      :to="`/${synonym}`"
+                    >
+                      {{ (idx == 0 ? '' : ', ') + synonym }}
+                    </router-link>
+                  </p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <h1>{{ dictionary.title }}</h1>
+      <h4>{{ dictionary.message }}</h4>
+      <p>{{ dictionary.resolution }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { fetchDictionary } from '@/utils/dictionary'
+import {
+  fetchDictionary,
+  Word,
+  isWordArray,
+  FailureMessage,
+} from '@/utils/dictionary'
 
 export default defineComponent({
   name: 'Word',
   data() {
     const word = this.$route.params.word as string
-    return {
-      dictionary: [],
+    const data: {
+      dictionary: any
+      isWord: boolean
+    } = {
+      dictionary: undefined,
+      isWord: true,
     }
+    return data
   },
   computed: {
     word() {
@@ -31,6 +79,18 @@ export default defineComponent({
   methods: {
     async loadDictionary(word: string) {
       this.dictionary = await fetchDictionary(word)
+    },
+    play(url: string) {
+      const audio = new Audio(url)
+      audio.play()
+    },
+    isWordArray(value: FailureMessage | Word[]): boolean {
+      return isWordArray(value)
+    },
+  },
+  watch: {
+    dictionary() {
+      this.isWord = isWordArray(this.dictionary)
     },
   },
 })
