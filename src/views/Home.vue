017a15e499c1824b358ec79c4900cd5b7727aaa9
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-04 20:32:00 +0100

    feat: new Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,39 @@
 <template>
-  <div></div>
+  <v-card class="px-6 py-3 home">
+    <h1>Free Dictionary</h1>
+    <v-divider horizontal class="mb-3" />
+    <p>
+      This website is created with <strong> Vue 3 </strong> and
+      <strong> Vuetify 3 (Alpha) </strong>.
+    </p>
+    <p>Works should be resumed after the official release of vuetify 3.</p>
+    <h2 class="mt-3">Thanks</h2>
+    <v-divider horizontal />
+    <p class="my-3">
+      Thanks to
+      <a href="https://github.com/meetDeveloper" target="_blank">
+        <img
+          class="rounded-circle"
+          src="https://avatars.githubusercontent.com/u/32453612"
+          alt="avatar"
+          height="24"
+          style="vertical-align: middle"
+        />
+        <strong> meetDeveloper </strong>
+      </a>
+      and other contributors for make
+      <a href="https://dictionaryapi.dev/" target="_blank">
+        <img
+          src="https://dictionaryapi.dev/favicon.ico"
+          alt="logo of free dictionary api"
+          height="24"
+          style="vertical-align: middle"
+        />
+        <strong> Free Dictionary API </strong>
+      </a>
+      which was used...
+    </p>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -12,3 +46,12 @@ export default defineComponent({
   }),
 })
 </script>
+
+<style>
+.thanks p {
+  font-size: 32px;
+}
+ul li {
+  margin-left: 2rem;
+}
+</style>
